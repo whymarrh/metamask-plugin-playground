b911c30dd159c9ad8918cc74491c7ac6b79c2be3
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-11-05 17:20:10 -0330

    count up from zero

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const isAscii = require('is-ascii');
 
+// TODO: Expose is-ascii to dapps?
+// TODO: TypeScript
+// TODO: Rimble design components for connect & hello
+
+let i = 0;
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
     switch (requestObject.method) {
         case 'hello':
+            i++;
             return wallet.send({
                 method: 'alert',
-                params: [`Hello, is '13' ASCII? ${
-                    isAscii('13')
+                params: [`Hello, is '${i}' ASCII? ${
+                    isAscii(String(i))
                         ? 'Yes'
                         : 'No'
                 }`]
