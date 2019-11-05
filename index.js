30e431295914e1d9eb7e42ee8bd3fed844bab1eb
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-11-05 16:24:49 -0330

    is-ascii 13

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const isAscii = require('is-ascii');
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
-  switch (requestObject.method) {
-    case 'hello':
-      return wallet.send({
-        method: 'alert',
-        params: [`Hello, ${originString}!`]
-      })
-    default:
-      throw new Error('Method not found.')
-  }
-})
+    switch (requestObject.method) {
+        case 'hello':
+            return wallet.send({
+                method: 'alert',
+                params: [`Hello, is '13' ASCII? ${
+                    isAscii('13')
+                        ? 'Yes'
+                        : 'No'
+                }`]
+            });
+        default:
+            throw new Error('Method not found.')
+    }
+});
